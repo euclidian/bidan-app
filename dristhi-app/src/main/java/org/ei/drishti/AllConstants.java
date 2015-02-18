e7743b75276ae911654fe922900cf9665c2ab1bd
Dimas Ciputra <dimas.ciputra@gmail.com>
2015-02-18 10:05:30 +0800

    DTC | Feature#6 | add constants and functions in context

--- dristhi-app/src/main/java/org/ei/drishti/AllConstants.java
+++ dristhi-app/src/main/java/org/ei/drishti/AllConstants.java
@@ -90,6 +90,8 @@ public class AllConstants {
         public static final String RECORD_ECPS = "record_ecps";
         public static final String FP_REFERRAL_FOLLOWUP = "fp_referral_followup";
         public static final String FP_FOLLOWUP = "fp_followup";
+        public static final String KARTU_IBU_REGISTRATION = "kartu_ibu";
+        public static final String KARTU_IBU_CLOSE = "";
     }
 
     public class ECRegistrationFields {
